24e33b90d2c97dea1cd0f630df89b1818836b6c8
EgorOrachyov <EgorOrachyov@users.noreply.github.com>
2023-04-03 06:54:05 +0000

    deploy: 75296db1497bcfefd89d621bfc4452954dbeacb8

--- docs-cpp/search/variables_3.js
+++ docs-cpp/search/variables_3.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['default_5falignment_1399',['DEFAULT_ALIGNMENT',['../classspla_1_1CLAllocLinear.html#a515b702a2177e38f918a770bb1c4fb3a',1,'spla::CLAllocLinear']]],
-  ['default_5fsize_1400',['DEFAULT_SIZE',['../classspla_1_1CLAllocLinear.html#af6129c707fd6c67b73e7157b6a9994b0',1,'spla::CLAllocLinear']]],
+  ['default_5falignment_1399',['DEFAULT_ALIGNMENT',['../classspla_1_1CLAllocLinear.html#a8465b53cf86cc35f62910fca95f93205',1,'spla::CLAllocLinear']]],
+  ['default_5fsize_1400',['DEFAULT_SIZE',['../classspla_1_1CLAllocLinear.html#a4c2a4c943034ca4c81fcd272fa8d58e6',1,'spla::CLAllocLinear']]],
   ['desc_1401',['desc',['../classspla_1_1ScheduleTaskBase.html#a6f755a549eb262b11d6aa025ef6673fc',1,'spla::ScheduleTaskBase']]],
   ['div_5ffloat_1402',['DIV_FLOAT',['../group__spla.html#ga46e6a6170d512189e8afc5854b81083c',1,'spla']]],
   ['div_5fint_1403',['DIV_INT',['../group__spla.html#ga72b019d06e6ae746236175aed02f0360',1,'spla']]],
