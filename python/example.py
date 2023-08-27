11fd31c32b868dc728ba016c0a2db598789bc096
Egor Orachev <egororachyov5@gmail.com>
2023-08-27 17:45:52 +0300

    gh-218: import C funcs into bridge

--- python/example.py
+++ python/example.py
@@ -1,2 +1,5 @@
-import spla
+import os
 
+os.environ["SPLA_PATH"] = "C:\\Users\\egoro\\Documents\\GitHub\\spla\\cmake-build-debug\\spla_x64.dll"
+
+import pyspla
--- python/pyspla/bridge.py
+++ python/pyspla/bridge.py
@@ -52,16 +52,52 @@ _p_object_t = None
 _callback_t = None
 _default_callback = None
 
+
+class SplaError(Exception):
+    pass
+
+
+class SplaNoAcceleration(SplaError):
+    pass
+
+
+class SplaPlatformNotFound(SplaError):
+    pass
+
+
+class SplaDeviceNotFound(SplaError):
+    pass
+
+
+class SplaInvalidState(SplaError):
+    pass
+
+
+class SplaInvalidArgument(SplaError):
+    pass
+
+
+class SplaNoValue(SplaError):
+    pass
+
+
+class SplaCompilationError(SplaError):
+    pass
+
+
+class SplaNotImplemented(SplaError):
+    pass
+
+
 _status_mapping = {
-    0: "OK",
-    1: "ERROR",
-    2: "NO_ACCELERATION",
-    3: "PLATFORM_NOT_FOUND",
-    4: "DEVICE_NOT_FOUND",
-    5: "INVALID_STATE",
-    6: "INVALID_ARGUMENT",
-    7: "NO_VALUE",
-    1024: "NOT_IMPLEMENTED",
+    1: SplaError,
+    2: SplaNoAcceleration,
+    3: SplaPlatformNotFound,
+    4: SplaDeviceNotFound,
+    5: SplaInvalidState,
+    6: SplaInvalidArgument,
+    7: SplaNoValue,
+    1024: SplaNotImplemented,
 }
 
 
@@ -76,6 +112,11 @@ def load_library(lib_path):
 
     _spla = ctypes.cdll.LoadLibrary(str(lib_path))
     _int = ctypes.c_int
+    _uint = ctypes.c_uint
+    _float = ctypes.c_float
+    _p_int = ctypes.POINTER(ctypes.c_int)
+    _p_uint = ctypes.POINTER(ctypes.c_uint)
+    _p_float = ctypes.POINTER(ctypes.c_float)
     _enum_t = ctypes.c_uint
     _status_t = ctypes.c_uint
     _object_t = ctypes.c_void_p
@@ -88,26 +129,255 @@ def load_library(lib_path):
                                    ctypes.c_int,
                                    ctypes.c_void_p)
 
-    _spla.spla_Library_finalize.restype = _status_t
+    _spla.spla_Library_finalize.restype = None
     _spla.spla_Library_finalize.argtypes = []
-
     _spla.spla_Library_set_accelerator.restype = _status_t
     _spla.spla_Library_set_accelerator.argtypes = [_enum_t]
-
     _spla.spla_Library_set_platform.restype = _status_t
     _spla.spla_Library_set_platform.argtypes = [_int]
-
     _spla.spla_Library_set_device.restype = _status_t
     _spla.spla_Library_set_device.argtypes = [_int]
-
     _spla.spla_Library_set_queues_count.restype = _status_t
     _spla.spla_Library_set_queues_count.argtypes = [_int]
-
     _spla.spla_Library_set_message_callback.restype = _status_t
     _spla.spla_Library_set_message_callback.argtypes = [_callback_t, ctypes.c_void_p]
-
     _spla.spla_Library_set_default_callback.restype = _status_t
     _spla.spla_Library_set_default_callback.argtypes = []
+    _spla.spla_Library_get_accelerator_info.restype = _status_t
+    _spla.spla_Library_get_accelerator_info.argtypes = [ctypes.c_char_p, _int]
+
+    _spla.spla_Type_int.restype = _object_t
+    _spla.spla_Type_int.argtypes = []
+    _spla.spla_Type_uint.restype = _object_t
+    _spla.spla_Type_uint.argtypes = []
+    _spla.spla_Type_float.restype = _object_t
+    _spla.spla_Type_float.argtypes = []
+
+    _spla.spla_OpBinary_PLUS_INT.restype = _object_t
+    _spla.spla_OpBinary_PLUS_UINT.restype = _object_t
+    _spla.spla_OpBinary_PLUS_FLOAT.restype = _object_t
+    _spla.spla_OpBinary_MINUS_INT.restype = _object_t
+    _spla.spla_OpBinary_MINUS_UINT.restype = _object_t
+    _spla.spla_OpBinary_MINUS_FLOAT.restype = _object_t
+    _spla.spla_OpBinary_MULT_INT.restype = _object_t
+    _spla.spla_OpBinary_MULT_UINT.restype = _object_t
+    _spla.spla_OpBinary_MULT_FLOAT.restype = _object_t
+    _spla.spla_OpBinary_DIV_INT.restype = _object_t
+    _spla.spla_OpBinary_DIV_UINT.restype = _object_t
+    _spla.spla_OpBinary_DIV_FLOAT.restype = _object_t
+    _spla.spla_OpBinary_MINUS_POW2_INT.restype = _object_t
+    _spla.spla_OpBinary_MINUS_POW2_UINT.restype = _object_t
+    _spla.spla_OpBinary_MINUS_POW2_FLOAT.restype = _object_t
+    _spla.spla_OpBinary_FIRST_INT.restype = _object_t
+    _spla.spla_OpBinary_FIRST_UINT.restype = _object_t
+    _spla.spla_OpBinary_FIRST_FLOAT.restype = _object_t
+    _spla.spla_OpBinary_SECOND_INT.restype = _object_t
+    _spla.spla_OpBinary_SECOND_UINT.restype = _object_t
+    _spla.spla_OpBinary_SECOND_FLOAT.restype = _object_t
+    _spla.spla_OpBinary_ONE_INT.restype = _object_t
+    _spla.spla_OpBinary_ONE_UINT.restype = _object_t
+    _spla.spla_OpBinary_ONE_FLOAT.restype = _object_t
+    _spla.spla_OpBinary_MIN_INT.restype = _object_t
+    _spla.spla_OpBinary_MIN_UINT.restype = _object_t
+    _spla.spla_OpBinary_MIN_FLOAT.restype = _object_t
+    _spla.spla_OpBinary_MAX_INT.restype = _object_t
+    _spla.spla_OpBinary_MAX_UINT.restype = _object_t
+    _spla.spla_OpBinary_MAX_FLOAT.restype = _object_t
+    _spla.spla_OpBinary_BOR_INT.restype = _object_t
+    _spla.spla_OpBinary_BOR_UINT.restype = _object_t
+    _spla.spla_OpBinary_BAND_INT.restype = _object_t
+    _spla.spla_OpBinary_BAND_UINT.restype = _object_t
+    _spla.spla_OpBinary_BXOR_INT.restype = _object_t
+    _spla.spla_OpBinary_BXOR_UINT.restype = _object_t
+
+    _spla.spla_OpBinary_PLUS_INT.argtypes = []
+    _spla.spla_OpBinary_PLUS_UINT.argtypes = []
+    _spla.spla_OpBinary_PLUS_FLOAT.argtypes = []
+    _spla.spla_OpBinary_MINUS_INT.argtypes = []
+    _spla.spla_OpBinary_MINUS_UINT.argtypes = []
+    _spla.spla_OpBinary_MINUS_FLOAT.argtypes = []
+    _spla.spla_OpBinary_MULT_INT.argtypes = []
+    _spla.spla_OpBinary_MULT_UINT.argtypes = []
+    _spla.spla_OpBinary_MULT_FLOAT.argtypes = []
+    _spla.spla_OpBinary_DIV_INT.argtypes = []
+    _spla.spla_OpBinary_DIV_UINT.argtypes = []
+    _spla.spla_OpBinary_DIV_FLOAT.argtypes = []
+    _spla.spla_OpBinary_MINUS_POW2_INT.argtypes = []
+    _spla.spla_OpBinary_MINUS_POW2_UINT.argtypes = []
+    _spla.spla_OpBinary_MINUS_POW2_FLOAT.argtypes = []
+    _spla.spla_OpBinary_FIRST_INT.argtypes = []
+    _spla.spla_OpBinary_FIRST_UINT.argtypes = []
+    _spla.spla_OpBinary_FIRST_FLOAT.argtypes = []
+    _spla.spla_OpBinary_SECOND_INT.argtypes = []
+    _spla.spla_OpBinary_SECOND_UINT.argtypes = []
+    _spla.spla_OpBinary_SECOND_FLOAT.argtypes = []
+    _spla.spla_OpBinary_ONE_INT.argtypes = []
+    _spla.spla_OpBinary_ONE_UINT.argtypes = []
+    _spla.spla_OpBinary_ONE_FLOAT.argtypes = []
+    _spla.spla_OpBinary_MIN_INT.argtypes = []
+    _spla.spla_OpBinary_MIN_UINT.argtypes = []
+    _spla.spla_OpBinary_MIN_FLOAT.argtypes = []
+    _spla.spla_OpBinary_MAX_INT.argtypes = []
+    _spla.spla_OpBinary_MAX_UINT.argtypes = []
+    _spla.spla_OpBinary_MAX_FLOAT.argtypes = []
+    _spla.spla_OpBinary_BOR_INT.argtypes = []
+    _spla.spla_OpBinary_BOR_UINT.argtypes = []
+    _spla.spla_OpBinary_BAND_INT.argtypes = []
+    _spla.spla_OpBinary_BAND_UINT.argtypes = []
+    _spla.spla_OpBinary_BXOR_INT.argtypes = []
+    _spla.spla_OpBinary_BXOR_UINT.argtypes = []
+
+    _spla.spla_OpSelect_EQZERO_INT.restype = _object_t
+    _spla.spla_OpSelect_EQZERO_UINT.restype = _object_t
+    _spla.spla_OpSelect_EQZERO_FLOAT.restype = _object_t
+    _spla.spla_OpSelect_NQZERO_INT.restype = _object_t
+    _spla.spla_OpSelect_NQZERO_UINT.restype = _object_t
+    _spla.spla_OpSelect_NQZERO_FLOAT.restype = _object_t
+    _spla.spla_OpSelect_GTZERO_INT.restype = _object_t
+    _spla.spla_OpSelect_GTZERO_UINT.restype = _object_t
+    _spla.spla_OpSelect_GTZERO_FLOAT.restype = _object_t
+    _spla.spla_OpSelect_GEZERO_INT.restype = _object_t
+    _spla.spla_OpSelect_GEZERO_UINT.restype = _object_t
+    _spla.spla_OpSelect_GEZERO_FLOAT.restype = _object_t
+    _spla.spla_OpSelect_LTZERO_INT.restype = _object_t
+    _spla.spla_OpSelect_LTZERO_UINT.restype = _object_t
+    _spla.spla_OpSelect_LTZERO_FLOAT.restype = _object_t
+    _spla.spla_OpSelect_LEZERO_INT.restype = _object_t
+    _spla.spla_OpSelect_LEZERO_UINT.restype = _object_t
+    _spla.spla_OpSelect_LEZERO_FLOAT.restype = _object_t
+    _spla.spla_OpSelect_ALWAYS_INT.restype = _object_t
+    _spla.spla_OpSelect_ALWAYS_UINT.restype = _object_t
+    _spla.spla_OpSelect_ALWAYS_FLOAT.restype = _object_t
+    _spla.spla_OpSelect_NEVER_INT.restype = _object_t
+    _spla.spla_OpSelect_NEVER_UINT.restype = _object_t
+    _spla.spla_OpSelect_NEVER_FLOAT.restype = _object_t
+
+    _spla.spla_OpSelect_EQZERO_INT.argtypes = []
+    _spla.spla_OpSelect_EQZERO_UINT.argtypes = []
+    _spla.spla_OpSelect_EQZERO_FLOAT.argtypes = []
+    _spla.spla_OpSelect_NQZERO_INT.argtypes = []
+    _spla.spla_OpSelect_NQZERO_UINT.argtypes = []
+    _spla.spla_OpSelect_NQZERO_FLOAT.argtypes = []
+    _spla.spla_OpSelect_GTZERO_INT.argtypes = []
+    _spla.spla_OpSelect_GTZERO_UINT.argtypes = []
+    _spla.spla_OpSelect_GTZERO_FLOAT.argtypes = []
+    _spla.spla_OpSelect_GEZERO_INT.argtypes = []
+    _spla.spla_OpSelect_GEZERO_UINT.argtypes = []
+    _spla.spla_OpSelect_GEZERO_FLOAT.argtypes = []
+    _spla.spla_OpSelect_LTZERO_INT.argtypes = []
+    _spla.spla_OpSelect_LTZERO_UINT.argtypes = []
+    _spla.spla_OpSelect_LTZERO_FLOAT.argtypes = []
+    _spla.spla_OpSelect_LEZERO_INT.argtypes = []
+    _spla.spla_OpSelect_LEZERO_UINT.argtypes = []
+    _spla.spla_OpSelect_LEZERO_FLOAT.argtypes = []
+    _spla.spla_OpSelect_ALWAYS_INT.argtypes = []
+    _spla.spla_OpSelect_ALWAYS_UINT.argtypes = []
+    _spla.spla_OpSelect_ALWAYS_FLOAT.argtypes = []
+    _spla.spla_OpSelect_NEVER_INT.argtypes = []
+    _spla.spla_OpSelect_NEVER_UINT.argtypes = []
+    _spla.spla_OpSelect_NEVER_FLOAT.argtypes = []
+
+    _spla.spla_Object_ref.restype = _status_t
+    _spla.spla_Object_unref.restype = _status_t
+
+    _spla.spla_Object_ref.argtypes = [_object_t]
+    _spla.spla_Object_unref.argtypes = [_object_t]
+
+    _spla.spla_Scalar_make.restype = _status_t
+    _spla.spla_Scalar_set_int.restype = _status_t
+    _spla.spla_Scalar_set_uint.restype = _status_t
+    _spla.spla_Scalar_set_float.restype = _status_t
+    _spla.spla_Scalar_get_int.restype = _status_t
+    _spla.spla_Scalar_get_uint.restype = _status_t
+    _spla.spla_Scalar_get_float.restype = _status_t
+
+    _spla.spla_Scalar_make.argtypes = [_p_object_t, _object_t]
+    _spla.spla_Scalar_set_int.argtypes = [_object_t, _int]
+    _spla.spla_Scalar_set_uint.argtypes = [_object_t, _uint]
+    _spla.spla_Scalar_set_float.argtypes = [_object_t, _float]
+    _spla.spla_Scalar_get_int.argtypes = [_object_t, _p_int]
+    _spla.spla_Scalar_get_uint.argtypes = [_object_t, _p_uint]
+    _spla.spla_Scalar_get_float.argtypes = [_object_t, _p_float]
+
+    _spla.spla_Array_make.restype = _status_t
+    _spla.spla_Array_set_int.restype = _status_t
+    _spla.spla_Array_set_uint.restype = _status_t
+    _spla.spla_Array_set_float.restype = _status_t
+    _spla.spla_Array_get_int.restype = _status_t
+    _spla.spla_Array_get_uint.restype = _status_t
+    _spla.spla_Array_get_float.restype = _status_t
+    _spla.spla_Array_clear.restype = _status_t
+
+    _spla.spla_Array_make.argtypes = [_p_object_t, _uint, _object_t]
+    _spla.spla_Array_set_int.argtypes = [_object_t, _uint, _int]
+    _spla.spla_Array_set_uint.argtypes = [_object_t, _uint, _uint]
+    _spla.spla_Array_set_float.argtypes = [_object_t, _uint, _float]
+    _spla.spla_Array_get_int.argtypes = [_object_t, _uint, _p_int]
+    _spla.spla_Array_get_uint.argtypes = [_object_t, _uint, _p_uint]
+    _spla.spla_Array_get_float.argtypes = [_object_t, _uint, _p_float]
+    _spla.spla_Array_clear.argtypes = [_object_t]
+
+    _spla.spla_Vector_make.restype = _status_t
+    _spla.spla_Vector_set_fill_value.restype = _status_t
+    _spla.spla_Vector_set_reduce.restype = _status_t
+    _spla.spla_Vector_set_int.restype = _status_t
+    _spla.spla_Vector_set_uint.restype = _status_t
+    _spla.spla_Vector_set_float.restype = _status_t
+    _spla.spla_Vector_get_int.restype = _status_t
+    _spla.spla_Vector_get_uint.restype = _status_t
+    _spla.spla_Vector_get_float.restype = _status_t
+    _spla.spla_Vector_build.restype = _status_t
+    _spla.spla_Vector_read.restype = _status_t
+    _spla.spla_Vector_clear.restype = _status_t
+
+    _spla.spla_Vector_make.argtypes = [_p_object_t, _uint, _object_t]
+    _spla.spla_Vector_set_fill_value.argtypes = [_object_t, _object_t]
+    _spla.spla_Vector_set_reduce.argtypes = [_object_t, _object_t]
+    _spla.spla_Vector_set_int.argtypes = [_object_t, _uint, _int]
+    _spla.spla_Vector_set_uint.argtypes = [_object_t, _uint, _uint]
+    _spla.spla_Vector_set_float.argtypes = [_object_t, _uint, _float]
+    _spla.spla_Vector_get_int.argtypes = [_object_t, _uint, _p_int]
+    _spla.spla_Vector_get_uint.argtypes = [_object_t, _uint, _p_uint]
+    _spla.spla_Vector_get_float.argtypes = [_object_t, _uint, _p_float]
+    _spla.spla_Vector_build.argtypes = [_object_t, _object_t, _object_t]
+    _spla.spla_Vector_read.argtypes = [_object_t, _object_t, _object_t]
+    _spla.spla_Vector_clear.argtypes = [_object_t]
+
+    _spla.spla_Matrix_make.restype = _status_t
+    _spla.spla_Matrix_set_fill_value.restype = _status_t
+    _spla.spla_Matrix_set_reduce.restype = _status_t
+    _spla.spla_Matrix_set_int.restype = _status_t
+    _spla.spla_Matrix_set_uint.restype = _status_t
+    _spla.spla_Matrix_set_float.restype = _status_t
+    _spla.spla_Matrix_get_int.restype = _status_t
+    _spla.spla_Matrix_get_uint.restype = _status_t
+    _spla.spla_Matrix_get_float.restype = _status_t
+    _spla.spla_Matrix_build.restype = _status_t
+    _spla.spla_Matrix_read.restype = _status_t
+    _spla.spla_Matrix_clear.restype = _status_t
+
+    _spla.spla_Matrix_make.argtypes = [_p_object_t, _uint, _uint, _object_t]
+    _spla.spla_Matrix_set_fill_value.argtypes = [_object_t, _object_t]
+    _spla.spla_Matrix_set_reduce.argtypes = [_object_t, _object_t]
+    _spla.spla_Matrix_set_int.argtypes = [_object_t, _uint, _uint, _int]
+    _spla.spla_Matrix_set_uint.argtypes = [_object_t, _uint, _uint, _uint]
+    _spla.spla_Matrix_set_float.argtypes = [_object_t, _uint, _uint, _float]
+    _spla.spla_Matrix_get_int.argtypes = [_object_t, _uint, _uint, _p_int]
+    _spla.spla_Matrix_get_uint.argtypes = [_object_t, _uint, _uint, _p_uint]
+    _spla.spla_Matrix_get_float.argtypes = [_object_t, _uint, _uint, _p_float]
+    _spla.spla_Matrix_build.argtypes = [_object_t, _object_t, _object_t, _object_t]
+    _spla.spla_Matrix_read.argtypes = [_object_t, _object_t, _object_t, _object_t]
+    _spla.spla_Matrix_clear.argtypes = [_object_t]
+
+    _spla.spla_Algorithm_bfs.restype = _status_t
+    _spla.spla_Algorithm_sssp.restype = _status_t
+    _spla.spla_Algorithm_pr.restype = _status_t
+    _spla.spla_Algorithm_tc.restype = _status_t
+
+    _spla.spla_Algorithm_bfs.argtypes = [_object_t, _object_t, _uint, _object_t]
+    _spla.spla_Algorithm_sssp.argtypes = [_object_t, _object_t, _uint, _object_t]
+    _spla.spla_Algorithm_pr.argtypes = [_p_object_t, _object_t, _float, _float, _object_t]
+    _spla.spla_Algorithm_tc.argtypes = [_p_int, _object_t, _object_t, _object_t]
 
 
 def default_callback(status, msg, file, function, line, user_data):
@@ -163,7 +433,7 @@ def initialize():
 
 def check(status):
     if status != 0:
-        raise Exception(f"{_status_mapping[status]}")
+        raise _status_mapping[status]
 
 
 # Initialize bridge on import
