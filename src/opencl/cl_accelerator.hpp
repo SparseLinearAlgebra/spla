18fe4fbd790499cfee030ae23d25a8b3c2627363
Vladimir Kutuev <vladimir.kutuev@gmail.com>
2025-08-19 14:21:55 +0300

    gh-235: add pocl cpu support

--- src/opencl/cl_accelerator.hpp
+++ src/opencl/cl_accelerator.hpp
@@ -46,10 +46,11 @@
 #define CL_HPP_TARGET_OPENCL_VERSION  120
 #include <CL/opencl.hpp>
 
-#define VENDOR_CODE_NVIDIA "nvidia"
-#define VENDOR_CODE_INTEL  "intel"
-#define VENDOR_CODE_AMD    "amd"
-#define VENDOR_CODE_IMG    "img"
+#define VENDOR_CODE_NVIDIA   "nvidia"
+#define VENDOR_CODE_INTEL    "intel"
+#define VENDOR_CODE_AMD      "amd"
+#define VENDOR_CODE_IMG      "img"
+#define VENDOR_CODE_POCL_CPU "pocl_cpu"
 
 namespace spla {
 
@@ -122,6 +123,7 @@ namespace spla {
         uint        m_default_wgs      = 64;
         uint        m_wave_size        = 32;
         uint        m_num_of_mem_banks = 32;
+        bool        m_is_pocl          = false;
         bool        m_is_nvidia        = false;
         bool        m_is_amd           = false;
         bool        m_is_intel         = false;
