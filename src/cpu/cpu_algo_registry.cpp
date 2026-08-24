f97555e2dbee98279db71efe39d2e758ee1f32fa
Tenyaeva Ekaterina <b08lsoai@gmail.com>
2026-08-24 16:43:19 +0300

    feat: cpu register v_assign_bslct for pair

--- src/cpu/cpu_algo_registry.cpp
+++ src/cpu/cpu_algo_registry.cpp
@@ -44,6 +44,7 @@
 #include <cpu/cpu_mxmT_masked.hpp>
 #include <cpu/cpu_mxv.hpp>
 #include <cpu/cpu_v_assign.hpp>
+#include <cpu/cpu_v_assign_bslct.hpp>
 #include <cpu/cpu_v_count_mf.hpp>
 #include <cpu/cpu_v_eadd.hpp>
 #include <cpu/cpu_v_eadd_fdb.hpp>
@@ -93,6 +94,9 @@ namespace spla {
         g_registry->add(MAKE_KEY_CPU_0("v_assign_masked", INT), std::make_shared<Algo_v_assign_masked_cpu<T_INT>>());
         g_registry->add(MAKE_KEY_CPU_0("v_assign_masked", UINT), std::make_shared<Algo_v_assign_masked_cpu<T_UINT>>());
         g_registry->add(MAKE_KEY_CPU_0("v_assign_masked", FLOAT), std::make_shared<Algo_v_assign_masked_cpu<T_FLOAT>>());
+
+        // algorthm v_assign_masked
+        g_registry->add(MAKE_KEY_CPU_0("v_assign_bslct_masked", PAIR), std::make_shared<Algo_v_assign_bslct_masked_cpu<T_PAIR>>());
 
         // algorthm m_reduce_by_row
         g_registry->add(MAKE_KEY_CPU_0("m_reduce_by_row", INT), std::make_shared<Algo_m_reduce_by_row_cpu<T_INT>>());
